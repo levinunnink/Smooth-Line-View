2278238b79c58d7bcbbc271c0c7ac763912be663
Alexis Gallagher <alexis@alexisgallagher.com>
2014-02-01 11:31:57 -0800

    whitespace

--- Smooth Line View/SmoothLineView.m
+++ Smooth Line View/SmoothLineView.m
@@ -32,15 +32,11 @@
 #define DEFAULT_BACKGROUND_COLOR    [UIColor whiteColor]
 
 static const CGFloat kPointMinDistance = 5;
-
 static const CGFloat kPointMinDistanceSquared = kPointMinDistance * kPointMinDistance;
 
-@interface SmoothLineView () 
-
+@interface SmoothLineView ()
 #pragma mark Private Helper function
-
 CGPoint midPoint(CGPoint p1, CGPoint p2);
-
 @end
 
 @implementation SmoothLineView
@@ -52,31 +48,31 @@ CGPoint midPoint(CGPoint p1, CGPoint p2);
 #pragma mark UIView lifecycle methods
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    
-    if (self) {
-        self.lineWidth = DEFAULT_WIDTH;
-        self.lineColor = DEFAULT_COLOR;
-        // NOTE: do not change the backgroundColor here, so it can be set in IB.
-        self.empty = YES;
+  self = [super initWithCoder:aDecoder];
+  
+  if (self) {
+    self.lineWidth = DEFAULT_WIDTH;
+    self.lineColor = DEFAULT_COLOR;
+    // NOTE: do not change the backgroundColor here, so it can be set in IB.
+    self.empty = YES;
 		path = CGPathCreateMutable();
-    }
-    
-    return self;
+  }
+  
+  return self;
 }
 
 - (id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    
-    if (self) {
-        self.lineWidth = DEFAULT_WIDTH;
-        self.lineColor = DEFAULT_COLOR;
-        self.backgroundColor = DEFAULT_BACKGROUND_COLOR;
-        self.empty = YES;
+  self = [super initWithFrame:frame];
+  
+  if (self) {
+    self.lineWidth = DEFAULT_WIDTH;
+    self.lineColor = DEFAULT_COLOR;
+    self.backgroundColor = DEFAULT_BACKGROUND_COLOR;
+    self.empty = YES;
 		path = CGPathCreateMutable();
-    }
-    
-    return self;
+  }
+  
+  return self;
 }
 
 - (void)drawRect:(CGRect)rect {
@@ -105,56 +101,56 @@ CGPoint midPoint(CGPoint p1, CGPoint p2);
 #pragma mark private Helper function
 
 CGPoint midPoint(CGPoint p1, CGPoint p2) {
-    return CGPointMake((p1.x + p2.x) * 0.5, (p1.y + p2.y) * 0.5);
+  return CGPointMake((p1.x + p2.x) * 0.5, (p1.y + p2.y) * 0.5);
 }
 
 #pragma mark Touch event handlers
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [touches anyObject];
-    
-    previousPoint1 = [touch previousLocationInView:self];
-    previousPoint2 = [touch previousLocationInView:self];
-    currentPoint = [touch locationInView:self];
-    
-    [self touchesMoved:touches withEvent:event];
+  UITouch *touch = [touches anyObject];
+  
+  previousPoint1 = [touch previousLocationInView:self];
+  previousPoint2 = [touch previousLocationInView:self];
+  currentPoint = [touch locationInView:self];
+  
+  [self touchesMoved:touches withEvent:event];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [touches anyObject];
+  UITouch *touch = [touches anyObject];
 	
 	CGPoint point = [touch locationInView:self];
 	
 	/* check if the point is farther than min dist from previous */
-    CGFloat dx = point.x - currentPoint.x;
-    CGFloat dy = point.y - currentPoint.y;
+  CGFloat dx = point.x - currentPoint.x;
+  CGFloat dy = point.y - currentPoint.y;
 	
-    if ((dx * dx + dy * dy) < kPointMinDistanceSquared) {
-        return;
-    }
-    
-    
-    previousPoint2 = previousPoint1;
-    previousPoint1 = [touch previousLocationInView:self];
-    currentPoint = [touch locationInView:self];
-    
-    CGPoint mid1 = midPoint(previousPoint1, previousPoint2);
-    CGPoint mid2 = midPoint(currentPoint, previousPoint1);
+  if ((dx * dx + dy * dy) < kPointMinDistanceSquared) {
+    return;
+  }
+  
+  
+  previousPoint2 = previousPoint1;
+  previousPoint1 = [touch previousLocationInView:self];
+  currentPoint = [touch locationInView:self];
+  
+  CGPoint mid1 = midPoint(previousPoint1, previousPoint2);
+  CGPoint mid2 = midPoint(currentPoint, previousPoint1);
 	CGMutablePathRef subpath = CGPathCreateMutable();
-    CGPathMoveToPoint(subpath, NULL, mid1.x, mid1.y);
-    CGPathAddQuadCurveToPoint(subpath, NULL, previousPoint1.x, previousPoint1.y, mid2.x, mid2.y);
-    CGRect bounds = CGPathGetBoundingBox(subpath);
+  CGPathMoveToPoint(subpath, NULL, mid1.x, mid1.y);
+  CGPathAddQuadCurveToPoint(subpath, NULL, previousPoint1.x, previousPoint1.y, mid2.x, mid2.y);
+  CGRect bounds = CGPathGetBoundingBox(subpath);
 	
 	CGPathAddPath(path, NULL, subpath);
 	CGPathRelease(subpath);
-    
-    CGRect drawBox = bounds;
-    drawBox.origin.x -= self.lineWidth * 2.0;
-    drawBox.origin.y -= self.lineWidth * 2.0;
-    drawBox.size.width += self.lineWidth * 4.0;
-    drawBox.size.height += self.lineWidth * 4.0;
-    
-    [self setNeedsDisplayInRect:drawBox];
+  
+  CGRect drawBox = bounds;
+  drawBox.origin.x -= self.lineWidth * 2.0;
+  drawBox.origin.y -= self.lineWidth * 2.0;
+  drawBox.size.width += self.lineWidth * 4.0;
+  drawBox.size.height += self.lineWidth * 4.0;
+  
+  [self setNeedsDisplayInRect:drawBox];
 }
 
 
